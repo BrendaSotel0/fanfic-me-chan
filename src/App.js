1d54d15192cad466f41560239428799c51cc1dce
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-19 17:50:21 -0600

    Stop for the night

--- src/App.js
+++ src/App.js
@@ -1,20 +1,41 @@
+import React from "react";
 import './App.css';
+import { Route } from "react-router-dom";
+import quoteData from "./APIcalls";
 
-function App() {
-
-  const fetchData = () => {
-    return fetch('https://animechan.vercel.app/api/random')
-    .then(response => response.json())
-    .then(quote => console.log(quote))
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      quotes: [],
+      error: '',
+      hasError: false,
+    };
   }
+
+  componentDidMount = () => {
+    return quoteData()
+      .then((data) => this.setState({ quotes: data }))
+      .catch((error) =>
+        this.setState({ hasError: true, error: `Oops! Something went wrong!` })
+      );
+  };
+
+  //  fetchData = () => {
+  //   return fetch('https://animechan.vercel.app/api/random')
+  //   .then(response => response.json())
+  //   .then(quote => console.log(quote))
+  // }
   
-  return (
-    <div className="App">
-      <header className="App-header">
-        <button onClick={() => fetchData()}>CLICK</button>
-      </header>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <button onClick={() => {this.componentDidMount()}}>CLICK</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
