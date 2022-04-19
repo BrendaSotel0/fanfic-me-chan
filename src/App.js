89802e66ea78d4bd419a0a3bb389f98f7bea7938
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-19 17:28:35 -0600

    Add inital setup

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom'
 import './App.css';
 
 function App() {
+
+  const fetchData = () => {
+    return fetch('https://animechan.vercel.app/api/random')
+    .then(response => response.json())
+    .then(quote => console.log(quote))
+  }
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => fetchData()}>CLICK</button>
       </header>
     </div>
   );
