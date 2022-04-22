73d225285694be9a04a490fa4fe2b1101fe3f1aa
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-22 16:48:05 -0600

    Add initial Cypress tests

---
+++ cypress/integration/load_spec.js
@@ -0,0 +1,13 @@
+describe('Load', () => {
+  it('Should have a header that says FanFic Me Chan', () => {
+    cy.visit("http://localhost:3000/").contains("FanFic Me Chan");
+  });
+
+  it('should have an aside navigation that greets you', () => {
+    cy.visit("http://localhost:3000/").contains("Welcome to FanFic Me Chan, the muse for your next fanfic adventure!");
+  })
+
+  
+
+
+})
