49a28cd48382899f83fc4c95d95a222f41f96bef
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-22 17:44:16 -0600

    Add quote page tests

--- cypress/integration/quote_spec.js
+++ cypress/integration/quote_spec.js
@@ -1,15 +1,11 @@
-describe('Load', () => {
+describe("Quote", () => {
   it('Should have a header that says FanFic Me Chan', () => {
-    cy.visit("http://localhost:3000/").contains("FanFic Me Chan");
-  });
-
-  it('Should link to a fetch', () => {
     cy.intercept('GET', 'https://animechan.vercel.app/api/random', {
       statusCode: 200,
       // fixture: 'questions.json'
-    }).as('something')
-    cy.visit('http://localhost:3000/')
-  })
+      }).as('something')
+      cy.visit("http://localhost:3000/").contains("FanFic Me Chan");
+  });
 
   it('Should have an aside navigation that greets you', () => {
     cy.visit("http://localhost:3000/").contains("Welcome to FanFic Me Chan, the muse for your next fanfic adventure!");
@@ -33,4 +29,9 @@ describe('Load', () => {
       .should('have.text', 'New Quote')
   })
 
+  it('Should be able to click on new quote button', () => {
+    cy.visit('http://localhost:3000/')
+    cy.get('*[class^="click-for-quote"]')
+      .click({ force: true })
+  })
 })
