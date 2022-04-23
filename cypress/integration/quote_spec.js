0aa1ccaed4348238b1c210988ceed4ca1cfd2905
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-23 16:54:47 -0600

    Add click save test

--- cypress/integration/quote_spec.js
+++ cypress/integration/quote_spec.js
@@ -23,4 +23,22 @@ describe("Quote", () => {
       .should('have.text', 'Saved Quotes')
   })
 
+  it('Should have a save quote button', () => {
+    cy.visit('http://localhost:3000/random-quote')
+    cy.get('*[class^="save-quote"]')
+      .should('have.text', 'Save')
+  })
+
+  // it('should be able to save the question to their game', () => {
+  //   cy.intercept('GET', 'https://animechan.vercel.app/api/random', {
+  //     statusCode: 200,
+  //     fixture: 'quote.json'
+  //   })
+  //   cy.visit('http://localhost:3000/')
+  //     cy.get('*[class^="arts-and-literature"]')
+  //     .click({ force: true})
+  //     cy.get('*[class^="like-button"]')
+  //     .click({ multiple: true, force: true })
+  //   })
+
 })
