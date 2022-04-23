64a05a8c33ff76aca83e9e84cabf4ccb9270d413
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-23 16:46:39 -0600

    POM

--- cypress/integration/saved_spec.js
+++ cypress/integration/saved_spec.js
@@ -1,26 +1,31 @@
-describe("Quote", () => {
+describe('Load', () => {
   it('Should have a header that says FanFic Me Chan', () => {
+    cy.visit("http://localhost:3000/saved").contains("FanFic Me Chan");
+  });
+
+  it('Should link to a fetch', () => {
     cy.intercept('GET', 'https://animechan.vercel.app/api/random', {
       statusCode: 200,
       fixture: 'quote.json'
-      })
-      cy.visit("http://localhost:3000/random-quote").contains("FanFic Me Chan");
-  });
+    })
+    cy.visit('http://localhost:3000/saved')
+  })
 
   it('Should have an aside navigation that greets you', () => {
-    cy.visit("http://localhost:3000/random-quote").contains("Welcome to FanFic Me Chan, the muse for your next fanfic adventure!");
+    cy.visit("http://localhost:3000/saved").contains("Welcome to FanFic Me Chan, the muse for your next fanfic adventure!");
   })
 
   it('Should have a home button', () => {
-    cy.visit('http://localhost:3000/random-quote')
+    cy.visit('http://localhost:3000/saved')
     cy.get('*[class^="home"]')
       .should('have.text', 'Home')
   })
 
   it('Should have a saved quotes button', () => {
-    cy.visit('http://localhost:3000/random-quote')
+    cy.visit('http://localhost:3000/saved')
     cy.get('*[class^="saved-quotes"]')
       .should('have.text', 'Saved Quotes')
   })
+
 
 })
