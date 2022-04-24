7d1493ea0dfcd01586929b19f3219d83b031448d
Brenda Sotelo <neverthelessitstrue@yahoo.com>
2022-04-24 15:48:08 -0600

    Add test to show saved section

--- cypress/integration/saved_spec.js
+++ cypress/integration/saved_spec.js
@@ -1,31 +1,47 @@
-describe('Load', () => {
-  it('Should have a header that says FanFic Me Chan', () => {
+describe("Load", () => {
+  it("Should have a header that says FanFic Me Chan", () => {
     cy.visit("http://localhost:3000/saved").contains("FanFic Me Chan");
   });
 
-  it('Should link to a fetch', () => {
-    cy.intercept('GET', 'https://animechan.vercel.app/api/random', {
+  it("Should link to a fetch", () => {
+    cy.intercept("GET", "https://animechan.vercel.app/api/random", {
       statusCode: 200,
-      fixture: 'quote.json'
-    })
-    cy.visit('http://localhost:3000/saved')
-  })
+      fixture: "quote.json",
+    });
+    cy.visit("http://localhost:3000/saved");
+  });
 
-  it('Should have an aside navigation that greets you', () => {
-    cy.visit("http://localhost:3000/saved").contains("Welcome to FanFic Me Chan, the muse for your next fanfic adventure!");
-  })
+  it("Should have an aside navigation that greets you", () => {
+    cy.visit("http://localhost:3000/saved").contains(
+      "Welcome to FanFic Me Chan, the muse for your next fanfic adventure!"
+    );
+  });
 
-  it('Should have a home button', () => {
-    cy.visit('http://localhost:3000/saved')
-    cy.get('*[class^="home"]')
-      .should('have.text', 'Home')
-  })
+  it("Should have a home button", () => {
+    cy.visit("http://localhost:3000/saved");
+    cy.get('*[class^="home"]').should("have.text", "Home");
+  });
 
-  it('Should have a saved quotes button', () => {
-    cy.visit('http://localhost:3000/saved')
+  it("Should have a saved quotes button", () => {
+    cy.visit("http://localhost:3000/saved");
+    cy.get('*[class^="saved-quotes"]').should("have.text", "Saved Quotes");
+  });
+
+  it("should show the saved quotes section", () => {
+    cy.visit("http://localhost:3000/");
+    cy.get('*[class^="click-for-quote"]')
+      .should("be.visible")
+      .click({ force: true });
+    cy.get('*[class^="save-quote"]')
+      .should("be.visible")
+      .click({ force: true });
+    cy.intercept({ method: "GET", path: "" }, { fixture: "quote.json" });
+    cy.request("http://localhost:3000/random-quote");
+    cy.get('*[class^="save-quote"]').click({ multiple: true, force: true });
     cy.get('*[class^="saved-quotes"]')
-      .should('have.text', 'Saved Quotes')
-  })
-
-
-})
+      .should("be.visible")
+      .click({ force: true });
+    cy.request("http://localhost:3000/saved");
+    cy.get('*[class^="saved-section"]').should("be.visible");
+  });
+});
